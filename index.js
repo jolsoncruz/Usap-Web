1d06602e3a3f26e15ba2eb7c61dc65ee65c55c17
Jolson Cruz <56365930+jolsoncruz@users.noreply.github.com>
2021-05-16 15:35:12 +0800

    First Iteration of Index.js
    > Fixed Package Dependencies
    > Fixed Env Configurations
    > Added Layouts and Partials Folder

--- index.js
+++ index.js
@@ -1,55 +1,74 @@
-// * EXPRESS APP
+// Package Dependencies
 const express = require('express');
+const path = require('path');
+const hbs = require('express-handlebars');
 const app = express();
-var path = require('path');
-app.use(express.static(path.join(__dirname, 'public')));
-
-// * VIEW ENGINE
-app.set('view engine', 'hbs');
-
-// * EXPRESS SESSIONS
+const mongoose = require('mongoose');
+const bodyparser = require("body-parser")
 const session = require('express-session')
+const bcrypt = require('bcrypt');
+const urlencoder = bodyparser.urlencoded({extended: false})
+
+// Import Models
+const userModel = require('./models/user');
+
+// Environment Configurations
+app.set('view engine', 'hbs');
+app.set('port', (process.env.PORT || 3000));
+app.engine('hbs',hbs({
+    extname: 'hbs',
+    defaultView: 'main',
+    layoutsDir: __dirname + '/views/layouts/',
+    partialsDir: __dirname + '/views/partials/',
+    helpers: {
+        ifCond: function(v1, v2, options) {
+          if(v1 === v2) {
+            return options.fn(this);
+          }
+          return options.inverse(this);
+        },
+        json: function(context) {
+            return JSON.stringify(context);
+        }
+    }
+}));
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(session({
     secret: "very super secret",
-    resave: false,
+    resave: true,
     saveUninitialized: true,
     cookie: {
         maxAge:1000*60*60*24*30,
         httpOnly: true
     }
-}))
+}));
 
-// * COOKIES
-const cookieparser = require("cookie-parser")
-app.use(cookieparser())
-
-// * BODY PARSER - URL ENCODER
-const bodyparser = require("body-parser")
-const urlencoder = bodyparser.urlencoded({extended: false})
-const { raw } = require('body-parser');
-
-// * MONGODB - MONGOOSE
-const mongoose = require('mongoose');
-const userModel = require('./models/user');
-
-// * IF NOT DEPLOYED THEN USE LOCALHOST
-const deployed = false; // todo: change to true when deployed
-if(deployed == false){
-    const port = 3000;
-    app.listen(port, () => {
-        console.log(`De La Salle Usap listening at http://localhost:${port}`)
-    })
-} else {
-    app.listen(process.env.PORT || 3000) // HEROKU STATEMENT
-}
+// Configuration for handling API endpoint data
+app.use(bodyParser.json()); // support json encoded bodies
+app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
 
 // * LANDING PAGE
 app.get('/', (req, res) => {
     res.render('index');
 });
 
-// * 404 PAGE
-app.use((req, res) => {
-    console.log("ERROR: 404");
-    //res.status(404).render('404'); // todo: change to this after creating 404 page
-})
+//HTTP Status Routes
+// app.use((req, res, next) => {
+//     res.status(404).render('frontend/error',{
+//         session: req.session,
+//         error: '404',
+//         message: "The page can't be found"
+//     });
+// });
+
+// app.use((req, res, next) => {
+//   res.status(500).render('frontend/error',{
+//     session: req.session,
+//     error: '500',
+//     message: 'Internal Server Error'
+//   });
+// });
+
+app.listen(app.get('port'), function(){
+    console.log('Server started on port ' + app.get('port'));
+});
