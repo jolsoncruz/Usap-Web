812385eade514549f982476c1414511aa228cfc3
Aryan Arora <aryanxarora@gmail.com>
2021-06-02 18:10:33 +0800

    io

--- public/js/client.js
+++ public/js/client.js
@@ -1,4 +1,4 @@
-import io from 'socket.io-client'
+const io = require('socket.io')(server, {cors:{origin:"*"}})
 const socket = io()
 const messageContainer = document.getElementById('message-container')
 const messageForm = document.getElementById('send-container')
